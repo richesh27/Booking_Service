9f28df7b8fe3aef4e37b8d3863b941e9eb30515b
richesh27 <kunwarrichesh@gmail.com>
2023-05-21 18:03:47 +0530

    added booking service server and folder structure

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+const {PORT} = require('./config/server-config');
+const SetupAndStartServer = async ()=>{
+
+const db = require('./models/index')
+const ApiRoutes = require('./routes/index');
+
+    app.listen(PORT,()=>{
+        
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({extended:true}))
+        
+        app.use('/api',ApiRoutes);
+
+        console.log(`Server is running on PORT ${PORT}`)
+
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter: true});
+        }
+    })
+}
+
+SetupAndStartServer(); 
---
+++ src/routes/v1/index.js
@@ -0,0 +1,8 @@
+const express = require ('express');
+
+const router = express.Router();
+
+//here goes the corresponding routes
+
+
+module.exports = router;
